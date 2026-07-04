da9ee24c72e0729c6d38c7ea3ffb23cb3d962952
sehgaamit-design <sehgaamit@gmail.com>
2026-07-05 01:52:16 +0530

    add css

--- src/pages/Shophome.jsx
+++ src/pages/Shophome.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Hero from '../components/Hero/Hero'
 import Popular from '../components/popular/Popular'
+import Offers from '../components/Offers/Offers'
+import Newcollection from '../components/New-collections/Newcollection'
 
 const Shophome = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers />
+      <Newcollection />
     </div>
   )
 }
